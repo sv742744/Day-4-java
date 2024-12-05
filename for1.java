6436f39593c36acb149998e55b9dbe10f01e57a6
sv742744 <shashi09op@gmail.com>
2024-12-05 12:28:53 +0530

    Add files via upload

---
+++ for1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+ public class for1 {
+    public static void main (String args[]){
+        Scanner s= new Scanner(System.in);
+        int d=s.nextInt();
+        for(int j=0;j<d;j++){
+            System.out.println("i="+j);
+        }
+    }
+ }
