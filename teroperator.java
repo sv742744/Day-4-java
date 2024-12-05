4e9afa4dcd3a5292d91bd2bf16f4059fcffbac8d
sv742744 <shashi09op@gmail.com>
2024-12-05 12:38:55 +0530

    Add files via upload

---
+++ teroperator.java
@@ -0,0 +1,9 @@
+import java.util.*;
+ public class teroperator {
+    public static void main (String args[]){
+        Scanner s= new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        System.out.println((a<b)?"a is smallest":"b is smallest");
+    }
+ }
