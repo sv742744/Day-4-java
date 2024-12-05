e59e3ecfdb5717b6e3b982b45ccc954fc88d823d
sv742744 <shashi09op@gmail.com>
2024-12-05 16:03:33 +0530

    Add files via upload

---
+++ incdec.java
@@ -0,0 +1,10 @@
+import java.util.*;
+ public class incdec {
+    public static void main (String args[]){
+        int s=5;
+        System.out.println(s++);// post increamnet//
+        System.out.println(++s);//pre increament//
+        System.out.println(--s);//pre decrreament//
+        System.out.println(s--);//post decreament//
+    }
+ }
